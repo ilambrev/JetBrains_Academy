7cee761fa3232dc191c522ad4a55ff4a1d2dd261
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-05-05 10:55:52 +0300

    Add files via upload

---
+++ Java_For_Beginners/Projects/01Chatty_Bot/ChattyBotST01.java
@@ -0,0 +1,8 @@
+package P01ChattyBot;
+
+public class ChattyBotST01 {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Johnnie.");
+        System.out.print("I was created in 2022.");
+    }
+}
